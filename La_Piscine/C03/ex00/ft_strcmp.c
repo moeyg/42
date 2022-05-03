f58dbeef07e173c84568e1bef5b567312e4a9c0e
moeyg <moooeyg@gmail.com>
2022-05-03 10:00:00 +0900

    Add C03-ex00

---
+++ La_Piscine/C03/ex00/ft_strcmp.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dopark <dopark@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/16 16:58:08 by dopark            #+#    #+#             */
+/*   Updated: 2022/02/17 18:44:25 by dopark           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	index;
+
+	index = 0;
+	while (s1[index] != '\0' || s2[index] != '\0')
+	{
+		if (s1[index] != s2[index])
+			return ((unsigned char)s1[index] - (unsigned char)s2[index]);
+		index ++;
+	}
+	return (0);
+}
