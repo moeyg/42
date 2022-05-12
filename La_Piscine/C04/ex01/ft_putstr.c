424b917b89415b06ed287385035ac434f8a333fe
moeyg <moooeyg@gmail.com>
2022-05-12 10:00:00 +0900

    Add C04-ex01

---
+++ La_Piscine/C04/ex01/ft_putstr.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dopark <dopark@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/17 11:59:27 by dopark            #+#    #+#             */
+/*   Updated: 2022/02/17 12:01:41 by dopark           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char str)
+{
+	write(1, &str, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int	index;
+
+	index = 0;
+	while (str[index] != '\0')
+	{
+		ft_putchar(str[index]);
+		index ++;
+	}
+}	
