89e514d27b49c6ea2c2753be580627818247baf6
moeyg <moooeyg@gmail.com>
2022-05-21 10:00:00 +0900

    Add C05-ex03

---
+++ La_Piscine/C05/ex03/ft_recursive_power.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dopark <dopark@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/19 16:17:30 by dopark            #+#    #+#             */
+/*   Updated: 2022/02/19 16:27:53 by dopark           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_recursive_power(int nb, int power)
+{
+	if (power < 0)
+	{
+		return (0);
+	}
+	else if (power == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (nb * ft_recursive_power(nb, power - 1));
+	}
+}
