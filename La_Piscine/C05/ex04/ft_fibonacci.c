4ac646be147eb1d5a4c17a89a8dffc14d842b8f7
moeyg <moooeyg@gmail.com>
2022-05-22 10:00:00 +0900

    Add C05-ex04

---
+++ La_Piscine/C05/ex04/ft_fibonacci.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_fibonacci.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dopark <dopark@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/19 16:19:47 by dopark            #+#    #+#             */
+/*   Updated: 2022/02/20 14:43:44 by dopark           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_fibonacci(int index)
+{
+	if (index < 0)
+	{
+		return (-1);
+	}
+	else if (index == 0)
+	{
+		return (0);
+	}
+	else if (index == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (ft_fibonacci(index - 2) + ft_fibonacci(index - 1));
+	}
+}
