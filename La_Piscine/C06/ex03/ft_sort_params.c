bfeb24beaa00c2b9561ea7ed345377dc7dc5bee1
moeyg <moooeyg@gmail.com>
2022-05-30 10:00:00 +0900

    Add C06-ex03

---
+++ La_Piscine/C06/ex03/ft_sort_params.c
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dopark <dopakr@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/20 17:12:03 by dopark            #+#    #+#             */
+/*   Updated: 2022/02/21 20:51:39 by dopark           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_swap(char **str1, char **str2);
+char	ft_strcmp(char *str1, char *str2);
+int		ft_strlen(char *str);
+
+int	main(int argc, char **argv)
+{
+	int	i;
+	int	j;
+
+	i = 1;
+	while (i < argc)
+	{
+		j = i + 1;
+		while (j < argc)
+		{
+			if (ft_strcmp(argv[i], argv[j]) > 0)
+				ft_swap(&argv[i], &argv[j]);
+			++ j;
+		}
+		++ i;
+	}
+	i = 1;
+	while (i < argc)
+	{
+		write (1, argv[i], ft_strlen(argv[i]));
+		write (1, "\n", 1);
+		++ i;
+	}
+}
+
+void	ft_swap(char **str1, char **str2)
+{
+	char	*temp;
+
+	temp = *str1;
+	*str1 = *str2;
+	*str2 = temp;
+}
+
+char	ft_strcmp(char *str1, char *str2)
+{
+	while (*str1 || *str2)
+	{
+		if (*str1 != *str2)
+			return ((unsigned char)*str1 - (unsigned char)*str2);
+		++ str1;
+		++ str2;
+	}
+	return (0);
+}
+
+int	ft_strlen(char	*str)
+{
+	int	index;
+
+	index = 0;
+	while (str[index])
+	{
+		index ++;
+	}
+	return (index);
+}
