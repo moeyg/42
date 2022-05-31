639d1ef6a4901667a55778fb8175f29bbaa28921
moeyg <moooeyg@gmail.com>
2022-05-31 10:00:00 +0900

    Add C08-ex00

---
+++ La_Piscine/C08/ex00/ft.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dopark <dopark@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/21 16:25:41 by dopark            #+#    #+#             */
+/*   Updated: 2022/02/23 09:41:56 by dopark           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_H
+# define FT_H
+void	ft_putchar(char c);
+void	ft_swap(int *a, int *b);
+void	ft_putstr(char *str);
+int		ft_strlen(char *str);
+int		ft_strcmp(char *s1, char *s2);
+#endif
